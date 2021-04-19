0e36b2850b44ca08098dfc6c3242d8321f50413c
SaraMohamed127 <msara@gmail.com>
2021-04-19 19:37:24 +0200

    add product interface

---
+++ src/app/interfaces/api-iproduct.ts
@@ -0,0 +1,9 @@
+export interface ApiIproduct {  
+      id: number,
+        Title: string,
+        price: number,
+        description: string,
+        slug: string,
+        img: string,
+        stock: string
+}
